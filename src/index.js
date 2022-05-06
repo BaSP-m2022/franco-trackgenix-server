1f98cca36ea124232f34983b2824af4089203d0a
Franco Marini <franco.marini@radiumrocket.com>
2022-05-06 11:43:06 -0300

    BaSP: example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // use "import" to import libraries
 import express from 'express';
-
-// use "require" to import JSON files
-const admins = require('./data/admins.json');
+import adminController from './resources/admins';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,11 +11,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
-});
+app.get('/admins/:id', adminController.getAdminById);
+app.get('/admins', adminController.getAdminsByQuery);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
