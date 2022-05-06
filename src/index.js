e008eb6c5c89fb9b6544bd46bff72db86862b39d
Franco Marini <franco.marini@radiumrocket.com>
2022-05-06 10:19:12 -0300

    TG: example of commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // use "import" to import libraries
 import express from 'express';
+import * as adminController from './resources/admins';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
+// const admins = require('./data/admins.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,11 +14,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
-});
+app.get('/admins/:id', adminController.getAdminById);
+app.get('/admins', adminController.getAdminsByQuery);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
