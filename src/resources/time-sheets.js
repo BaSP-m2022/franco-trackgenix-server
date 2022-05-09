f6a76798df6242d1925f1e0aab76163fd436ef7d
Juan Diego Auil <juandiauil@gmail.com>
2022-05-09 15:30:55 -0300

    TG: filter process

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import express from 'express';
 import fs from 'fs';
 
@@ -20,50 +21,41 @@ router.get('/getByCheck/:check', (req, res) => {
   }
 });
 
-router.get('/getByManager', (req, res) => {
+router.get('/filter', (req, res) => {
   const timesheetManager = req.query.manager;
-  const filteredManager = timesheets.filter(
-    (timesheet) => timesheet.manager === timesheetManager,
-  );
-  if (filteredManager.length > 0) {
-    res.send(filteredManager);
-  } else {
-    res.send(`There are not ${timesheetManager}`);
-  }
-});
-
-router.get('/getByProject', (req, res) => {
   const timesheetProject = req.query.project;
-  const filteredProject = timesheets.filter(
-    (timesheet) => timesheet.project === timesheetProject,
-  );
-  if (filteredProject.length > 0) {
-    res.send(filteredProject);
-  } else {
-    res.send(`There are not ${timesheetProject}`);
-  }
-});
-
-router.get('/getByUser', (req, res) => {
   const timesheetUser = req.query.user;
-  const filteredUser = timesheets.filter(
-    (timesheet) => timesheet.user === timesheetUser,
-  );
-  if (filteredUser.length > 0) {
-    res.send(filteredUser);
-  } else {
-    res.send(`There are not ${timesheetUser}`);
-  }
-});
-router.get('/getByDay', (req, res) => {
   const timesheetDay = req.query.day;
-  const filteredDay = timesheets.filter(
-    (timesheet) => timesheet.day === timesheetDay,
-  );
-  if (filteredDay.length > 0) {
-    res.send(filteredDay);
-  } else {
-    res.send(`There are not ${timesheetDay}`);
+  if (!timesheetManager && !timesheetProject && !timesheetUser && !timesheetDay) {
+    res.send('There are not filter');
+  } else if (timesheetManager) {
+    const filteredManager = timesheets.filter((timesheet) => timesheet.manager.includes(timesheetManager));
+    if (filteredManager.length > 0) {
+      res.send(filteredManager);
+    } else {
+      res.send(`There are not ${timesheetManager}`);
+    }
+  } else if (timesheetProject) {
+    const filteredProject = timesheets.filter((timesheet) => timesheet.project.includes(timesheetProject));
+    if (filteredProject.length > 0) {
+      res.send(filteredProject);
+    } else {
+      res.send(`There are not ${timesheetProject}`);
+    }
+  } else if (timesheetUser) {
+    const filteredUser = timesheets.filter((timesheet) => timesheet.user.includes(timesheetUser));
+    if (filteredUser.length > 0) {
+      res.send(filteredUser);
+    } else {
+      res.send(`There are not ${timesheetUser}`);
+    }
+  } else if (timesheetDay) {
+    const filteredDay = timesheets.filter((timesheet) => timesheet.day.includes(timesheetDay));
+    if (filteredDay.length > 0) {
+      res.send(filteredDay);
+    } else {
+      res.send(`There are not ${timesheetDay}`);
+    }
   }
 });
 
