f8de227e4e11f820975317f7895f2bf24639ac00
Gonzalo Collares <collares.gonzalo@gmail.com>
2022-05-09 19:51:37 -0300

    TG: Create admin and get admin by id

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 // use "import" to import libraries
 import express from 'express';
-import adminController from './resources/admins';
+import adminRouter from './resources/admins';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/admins', adminRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/admins/:id', adminController.getAdminById);
-app.get('/admins', adminController.getAdminsByQuery);
+// app.get('/admins/:id', adminRouter.getAdminById);
+// app.get('/admins', adminRouter.getAdminsByQuery);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,15 +1,58 @@
-// use "require" to import JSON files
+import express from 'express';
+import fs from 'fs';
+
 const admins = require('../data/admins.json');
 
-const getAdminById = (req, res) => {
-  res.send(admins);
-};
+const router = express.Router();
 
-const getAdminsByQuery = (req, res) => {
-  res.send(admins);
-};
+router.post('/new', (req, res) => {
+  const adminData = req.body;
+  if (adminData.id
+    && adminData.firstName
+    && adminData.lastName
+    && adminData.email
+    && adminData.dateOfBirth
+    && adminData.password
+    && adminData.dni) {
+    admins.push(adminData);
+    fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('Admin created');
+      }
+    });
+  } else {
+    res.send('Admin not created, missing data');
+  }
+});
 
-export default {
-  getAdminById,
-  getAdminsByQuery,
-};
+router.get('/getAll', (req, res) => {
+  res.send(admins);
+});
+
+router.get('/getById/:id', (req, res) => {
+  const admId = req.params.id;
+  const admin = admins.find((adm) => adm.id === parseInt(admId, 10));
+  if (admin) {
+    res.send(admin);
+  } else {
+    res.send('Admin not found');
+  }
+});
+
+export default router;
+// module.exports = router;
+
+// const getAdminById = (req, res) => {
+//   res.send(admins);
+// };
+
+// const getAdminsByQuery = (req, res) => {
+//   res.send(admins);
+// };
+
+// export default {
+//   getAdminById,
+//   getAdminsByQuery,
+// };
