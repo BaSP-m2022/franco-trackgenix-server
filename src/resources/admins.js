fda517ef9d1cf66bf3df1b903440967a0baaa9e6
Gonzalo Collares <collares.gonzalo@gmail.com>
2022-05-09 21:33:46 -0300

    TG: edit admin

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -5,7 +5,7 @@ const admins = require('../data/admins.json');
 
 const router = express.Router();
 
-router.post('/new', (req, res) => {
+router.post('/create', (req, res) => {
   const adminData = req.body;
   if (adminData.id
     && adminData.firstName
@@ -27,13 +27,35 @@ router.post('/new', (req, res) => {
   }
 });
 
+router.put('/edit/:id', (req, res) => {
+  const adminId = req.params.id;
+  const adminToEdit = admins.find((adm) => adm.id === adminId);
+  if (adminToEdit) {
+    const adminBody = req.body;
+    adminToEdit.firstName = adminBody.firstName ? adminBody.firstName : adminToEdit.firstName;
+    adminToEdit.lastName = adminBody.lastName ? adminBody.lastName : adminToEdit.lastName;
+    adminToEdit.email = adminBody.email ? adminBody.email : adminToEdit.email;
+    // eslint-disable-next-line max-len
+    adminToEdit.dateOfBirth = adminBody.dateOfBirth ? adminBody.dateOfBirth : adminToEdit.dateOfBirth;
+    adminToEdit.password = adminBody.password ? adminBody.password : adminToEdit.password;
+    fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('Admin edited');
+      }
+    });
+  } else {
+    res.send('Admin not found');
+  }
+});
 router.get('/getAll', (req, res) => {
   res.send(admins);
 });
 
 router.get('/getById/:id', (req, res) => {
   const admId = req.params.id;
-  const admin = admins.find((adm) => adm.id === parseInt(admId, 10));
+  const admin = admins.find((adm) => adm.id === admId);
   if (admin) {
     res.send(admin);
   } else {
@@ -42,17 +64,3 @@ router.get('/getById/:id', (req, res) => {
 });
 
 export default router;
-// module.exports = router;
-
-// const getAdminById = (req, res) => {
-//   res.send(admins);
-// };
-
-// const getAdminsByQuery = (req, res) => {
-//   res.send(admins);
-// };
-
-// export default {
-//   getAdminById,
-//   getAdminsByQuery,
-// };
