e8ba6c3ed5084e508f3676e99f54e7ecb6532085
FlorenciaEtchart <floretchart04@gmail.com>
2022-05-11 00:42:37 +0200

    TG: work finish on port 3000

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import adminController from './resources/admins';
 import employeesRouter from './resources/employees';
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
