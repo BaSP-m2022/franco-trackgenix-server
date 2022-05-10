2db7ae4509c2d1f1a5619bbc43fce4c1f75651bb
FlorenciaEtchart <floretchart04@gmail.com>
2022-05-10 14:56:31 +0200

    TG: filter function to check

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,15 +1,19 @@
 import express from 'express';
+// eslint-disable-next-line no-unused-vars
+import res from 'express/lib/response';
 import fs from 'fs';
 
 const router = express.Router();
 const employees = require('../data/employees.json');
 
+// eslint-disable-next-line no-shadow
 router.get('/', (req, res) => {
   res.send(employees);
 });
 
 // get an employee by id
 
+// eslint-disable-next-line no-shadow
 router.get('/getById/:id', (req, res) => {
   const employeeId = req.params.id;
   // eslint-disable-next-line no-shadow
@@ -23,6 +27,7 @@ router.get('/getById/:id', (req, res) => {
 
 // delete an employee by id
 
+// eslint-disable-next-line no-shadow
 router.delete('/deleted/:id', (req, res) => {
   const employeeId = req.params.id;
   // eslint-disable-next-line no-shadow
@@ -68,24 +73,68 @@ router.delete('/deleted/:id', (req, res) => {
 
 // filter employee
 
-// router.get('/', (req, res) => {
-//     const employeeId = req.query.id;
-//     const employeeName = req.query.Name;
-//     const employeeLastName = req.query.lastName;
-//     const employeeEmail = req.query.email;
-//     const employeeDoB = req.query.dateOfBirth;
-//     const employeeDni = req.query.dni;
-//     if (!employeeId & !employeeName & !employeeLastName &!employeeEmail
-//     &!employeeDoB &!employeeDni) {
-//         res.send(employees);
-//     }
-//     const filterById = employees.filter((employee) => {
-//         if (employeeId & employeeName & employeeName & employeeLastName &employeeEmail &
-//         employeeDoB &employeeDni) {
-//             return employee.id.includes(employeeId) & employee.Name.includes(employeeName) &
-//         employee.Name.includes(employeeName)
-//         }
-//     }
-// })
+// eslint-disable-next-line no-shadow
+router.get('/filter', (req, res) => {
+  const employeeId = req.query.id;
+  const employeeName = req.query.Name;
+  const employeeLastName = req.query.lastName;
+  const employeeEmail = req.query.email;
+  const employeeDoB = req.query.dateOfBirth;
+  const employeeDni = req.query.dni;
+  if (!employeeId && !employeeName && !employeeLastName && !employeeEmail
+    && !employeeDoB && !employeeDni) {
+    res.send(employees);
+  }
+  const employeeFilter = employees.filter((employee) => {
+    if (employeeId && employeeName && employeeLastName && employeeEmail
+        && employeeDoB && employeeDni) {
+      return employee.id.includes(employeeId) && employee.Name.includes(employeeName)
+            // eslint-disable-next-line max-len
+            && employee.lastName.includes(employeeLastName) && employee.email.includes(employeeEmail)
+            && employee.dateOfBirth.includes(employeeDoB) && employee.dni.includes(employeeDni);
+    }
+    if (employeeName && employeeLastName && employeeEmail && employeeDoB && employeeDni) {
+      return employee.Name.includes(employeeName) && employee.lastName.includes(employeeLastName)
+            && employee.email.includes(employeeEmail) && employee.dateOfBirth.includes(employeeDoB)
+            && employee.dni.includes(employeeDni);
+    }
+    if (employeeLastName && employeeEmail && employeeDoB && employeeDni) {
+      return employee.lastName.includes(employeeLastName) && employee.email.includes(employeeEmail)
+            && employee.dateOfBirth.includes(employeeDoB) && employee.dni.includes(employeeDni);
+    }
+    if (employeeEmail && employeeDoB && employeeDni) {
+      return employee.email.includes(employeeEmail) && employee.dateOfBirth.includes(employeeDoB)
+            && employee.dni.includes(employeeDni);
+    }
+    if (employeeDoB && employeeDni) {
+      return employee.dateOfBirth.includes(employeeDoB) && employee.dni.includes(employeeDni);
+    }
+    if (employeeDni) {
+      return employee.dni.includes(employeeDni);
+    }
+    if (employeeDoB) {
+      return employee.dateOfBirth.includes(employeeDoB);
+    }
+    if (employeeEmail) {
+      return employee.email.includes(employeeEmail);
+    }
+    if (employeeLastName) {
+      return employee.lastName.includes(employeeLastName);
+    }
+    if (employeeName) {
+      return employee.Name.includes(employeeName);
+    }
+    if (employeeId) {
+      return employee.id.includes(employeeId);
+    }
+    return false;
+  });
+
+  if (employeeFilter.length > 0) {
+    res.send(employeeFilter);
+  } else {
+    res.send('Employee not found');
+  }
+});
 
 export default router;
