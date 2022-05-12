4c30b388e4caf40ea4fe324aa8124175771cd998
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2022-05-12 13:15:00 -0300

    TG: change location of router code lines and require

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import fs from 'fs';
 
-const router = express.Router();
-
 const projects = require('../data/projects.json');
+
+const router = express.Router();
 
 router.post('/', (req, res) => {
   const projectData = req.body;
