2fe6739a70ce70f35455bff2a097796868110fa5
Gonzalo Collares <collares.gonzalo@gmail.com>
2022-05-14 13:40:09 -0300

    TG: new branch and model created

---
+++ src/models/Time-sheets.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const timesheetsSchema = new mongoose.Schema({
+  task: {
+    type: Array,
+  },
+  totalHours: {
+    type: Number,
+  },
+  checked: {
+    type: Boolean,
+    required: true,
+  },
+  status: {
+    type: String,
+  },
+  startDate: {
+    type: Date,
+  },
+  endDate: {
+    type: Date,
+  },
+  projectId: {
+    type: String,
+  },
+  employeeId: {
+    type: String,
+  },
+  managerId: {
+    type: String,
+  },
+});
+
+export default mongoose.model('Time-sheets', timesheetsSchema);
