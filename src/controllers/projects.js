e9a83ed6f533bb0c29d9cecab5c24ed582af7584
NicolasFontana <nico.fontana12@gmail.com>
2022-05-16 16:11:59 -0300

    TG-8: create method with validations and getById

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,118 +1,203 @@
-// import Project from '../models/Project';
-// import fs from 'fs';
+import Project from '../models/Project';
 
 const projects = [];
 
-const create = (req, res) => {
-  const projectData = req.body;
-  if (projectData.id && projectData.name && projectData.status && projectData.description
-    && projectData.employees && projectData.startDate && projectData.endDate) {
-    projects.push(projectData);
-    // fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
-    //   if (err) {
-    //     res.send(err);
-    //   } else {
-    //     res.send('Project create');
-    //   }
-    // });
-  } else {
-    res.send('project not created');
+const create = async (req, res) => {
+  try {
+    const project = new Project({
+      name: req.body.name,
+      status: req.body.status,
+      description: req.body.description,
+      employees: req.body.employees,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+    });
+    await project.save();
+    return res.status(201).json({
+      message: 'Project was created.',
+      data: project,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      data: undefined,
+      error: true,
+    });
   }
 };
 
-const deleteById = (req, res) => {
+const deleteById = async (req, res) => {
   const projectId = req.params.id;
-  const filteredProjects = projects.filter((project) => projectId !== project.id);
-
-  if (projects.length === filteredProjects.length) {
-    res.send('Could not delete project because it was not found');
-  } else {
-    // fs.writeFile('src/data/projects.json', JSON.stringify(filteredProjects), (err) => {
-    //   if (err) {
-    //     res.send(err);
-    //   } else {
-    //     res.send('Project delete');
-    //   }
-    // });
+  console.log(projectId);
+  try {
+    if (!projectId) {
+      console.log('1');
+      return res.status(400).send({
+        message: 'Missing ID parameter',
+        data: undefined,
+        error: true,
+      });
+    }
+    console.log('2');
+    const searchProject = await Project.findByIdAndDelete(projectId);
+    return res.status(204).send({
+      message: 'Project was deleted',
+      data: searchProject,
+      error: false,
+    });
+  } catch (error) {
+    console.log('3');
+    return res.send({
+      message: error.message,
+      data: undefined,
+      error: true,
+    });
   }
+  // const projectId = req.params.id;
+  // const filteredProjects = projects.filter((project) => projectId !== project.id);
+
+  // if (projects.length === filteredProjects.length) {
+  //   res.send('Could not delete project because it was not found');
+  // } else {
+  //   // fs.writeFile('src/data/projects.json', JSON.stringify(filteredProjects), (err) => {
+  //   //   if (err) {
+  //   //     res.send(err);
+  //   //   } else {
+  //   //     res.send('Project delete');
+  //   //   }
+  //   // });
+  // }
 };
 
-const filter = (req, res) => {
+const filter = async (req, res) => {
   const pName = req.query.name;
   const pStatus = req.query.status;
   const pDescription = req.query.description;
   const pEmployees = req.query.employees;
   const pStartDate = req.query.startDate;
   const pEndDate = req.query.startDate;
-  if (!pName && !pStatus && !pDescription && !pEmployees && !pStartDate && !pEndDate) {
-    res.send(projects);
-  }
+  const projectX = await Project.find({ name: pName });
+  console.log('aca');
+  console.log(projectX);
+  try {
+    if (!pName && !pStatus && !pDescription && !pEmployees && !pStartDate && !pEndDate) {
+      const allProjects = await Project.find({});
+      console.log('0');
+      return res.status(201).json({
+        message: '',
+        data: allProjects,
+        error: false,
+      });
+    }
 
-  const filteredProjects = projects.filter((p) => {
-    if (pName && pStatus && pDescription && pEmployees && pStartDate && pEndDate) {
-      return p.name.toLowerCase().includes(pName.toLowerCase())
-            && p.status.toLowerCase().includes(pStatus.toLowerCase())
-            && p.description.toLowerCase().includes(pDescription.toLowerCase())
-            && p.employees.toLowerCase().includes(pEmployees.toLowerCase())
-            && p.startDate.toLowerCase().includes(pStartDate.toLowerCase())
-            && p.endDate.toLowerCase().includes(pEndDate.toLowerCase());
-    }
-    if (pName && pStatus && pDescription && pEmployees && pStartDate) {
-      return p.name.toLowerCase().includes(pName.toLowerCase())
-      && p.status.toLowerCase().includes(pStatus.toLowerCase())
-      && p.description.toLowerCase().includes(pDescription.toLowerCase())
-      && p.employees.toLowerCase().includes(pEmployees.toLowerCase())
-      && p.startDate.toLowerCase().includes(pStartDate.toLowerCase());
-    }
-    if (pName && pStatus && pDescription && pEmployees) {
-      return p.name.toLowerCase().includes(pName.toLowerCase())
-      && p.status.toLowerCase().includes(pStatus.toLowerCase())
-      && p.description.toLowerCase().includes(pDescription.toLowerCase())
-      && p.employees.toLowerCase().includes(pEmployees.toLowerCase());
-    }
-    if (pName && pStatus && pDescription) {
-      return p.name.toLowerCase().includes(pName.toLowerCase())
-      && p.status.toLowerCase().includes(pStatus.toLowerCase())
-      && p.description.toLowerCase().includes(pDescription.toLowerCase());
-    }
+    // if (pName && pStatus && pDescription && pEmployees && pStartDate && pEndDate) {
+    //   return p.name.toLowerCase().includes(pName.toLowerCase())
+    //           && p.status.toLowerCase().includes(pStatus.toLowerCase())
+    //           && p.description.toLowerCase().includes(pDescription.toLowerCase())
+    //           && p.employees.toLowerCase().includes(pEmployees.toLowerCase())
+    //           && p.startDate.toLowerCase().includes(pStartDate.toLowerCase())
+    //           && p.endDate.toLowerCase().includes(pEndDate.toLowerCase());
+    // }
+    // if (pName && pStatus && pDescription && pEmployees && pStartDate) {
+    //   return p.name.toLowerCase().includes(pName.toLowerCase())
+    //     && p.status.toLowerCase().includes(pStatus.toLowerCase())
+    //     && p.description.toLowerCase().includes(pDescription.toLowerCase())
+    //     && p.employees.toLowerCase().includes(pEmployees.toLowerCase())
+    //     && p.startDate.toLowerCase().includes(pStartDate.toLowerCase());
+    // }
+    // if (pName && pStatus && pDescription && pEmployees) {
+    //   return p.name.toLowerCase().includes(pName.toLowerCase())
+    //     && p.status.toLowerCase().includes(pStatus.toLowerCase())
+    //     && p.description.toLowerCase().includes(pDescription.toLowerCase())
+    //     && p.employees.toLowerCase().includes(pEmployees.toLowerCase());
+    // }
+    // if (pName && pStatus && pDescription) {
+    //   return p.name.toLowerCase().includes(pName.toLowerCase())
+    //     && p.status.toLowerCase().includes(pStatus.toLowerCase())
+    //     && p.description.toLowerCase().includes(pDescription.toLowerCase());
+    // }
     if (pName && pStatus) {
-      return p.name.toLowerCase().includes(pName.toLowerCase())
-      && p.status.toLowerCase().includes(pStatus.toLowerCase());
+      console.log('1');
+      const project = await Project.find({ name: pName, status: pStatus });
+      return res.send({
+        message: 'Found projects',
+        data: project,
+        error: false,
+      });
     }
     if (pName) {
-      return p.name.toLowerCase().includes(pName.toLowerCase());
+      console.log('2');
+      const project = await Project.find({ name: pName });
+      console.log(project);
+      return res.send({
+        message: 'Found projects',
+        data: project,
+        error: false,
+      });
     }
-    if (pStatus) {
-      return p.status.toLowerCase().includes(pStatus.toLowerCase());
-    }
-    if (pDescription) {
-      return p.description.toLowerCase().includes(pDescription.toLowerCase());
-    }
-    if (pEmployees) {
-      return p.employees.toLowerCase().includes(pEmployees.toLowerCase());
-    }
-    if (pStartDate) {
-      return p.startDate.toLowerCase().includes(pStartDate.toLowerCase());
-    }
-    if (pEndDate) {
-      return p.endDate.toLowerCase().includes(pEndDate);
-    }
-    return false;
-  });
-  if (filteredProjects.length > 0) {
-    res.send(filteredProjects);
-  } else {
-    res.send('Project not found');
+    // if (pStatus) {
+    //   return p.status.toLowerCase().includes(pStatus.toLowerCase());
+    // }
+    // if (pDescription) {
+    //   return p.description.toLowerCase().includes(pDescription.toLowerCase());
+    // }
+    // if (pEmployees) {
+    //   return p.employees.toLowerCase().includes(pEmployees.toLowerCase());
+    // }
+    // if (pStartDate) {
+    //   return p.startDate.toLowerCase().includes(pStartDate.toLowerCase());
+    // }
+    // if (pEndDate) {
+    //   return p.endDate.toLowerCase().includes(pEndDate);
+    // }
+    // return false;
+
+    // if (filteredProjects.length > 0) {
+    //   res.send(filteredProjects);
+    // } else {
+    //   res.send('Project not found');
+    // }
+    // if (project.length === 0) {
+    //   return res.send({
+    //     message: 'Project not found',
+    //     data: undefined,
+    //     error: true,
+    //   });
+    // }
+  } catch (error) {
+    console.log('3');
+    return res.send({
+      message: error.message,
+      data: undefined,
+      error: true,
+    });
   }
+  return false;
 };
 
-const getById = (req, res) => {
+const getById = async (req, res) => {
   const projectId = req.params.id;
-  const project = projects.find((s) => s.id === projectId);
-  if (project) {
-    res.send(project);
-  } else {
-    res.send('Project not found');
+  try {
+    if (projectId) {
+      const project = await Project.findById(projectId);
+      return res.status(200).json({
+        message: `Project with ID ${projectId} found`,
+        data: project,
+        error: false,
+      });
+    }
+    return res.status(400).json({
+      message: 'Missing ID parameter',
+      data: undefined,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: 'Project not found in database',
+      data: undefined,
+      error: true,
+    });
   }
 };
 
---
+++ src/validations/project.js
@@ -0,0 +1,30 @@
+import Joi from 'joi';
+
+const validationCreateProject = (req, res, next) => {
+  const employeesSchema = Joi.object({
+    rate: Joi.number().required().greater(0),
+    role: Joi.string().required().valid('agregar'),
+  });
+
+  const projectValidation = Joi.object({
+    name: Joi.string().required().min(3),
+    status: Joi.string().valid('active', 'inactive'),
+    description: Joi.string().min(10).max(100),
+    employees: Joi.array().items(employeesSchema),
+    startDate: Joi.date().less('now').required(),
+    endDate: Joi.date().greater('now').optional(),
+  });
+  const validation = projectValidation.validate(req.body);
+  if (validation.error) {
+    return res.status(400).json({
+      message: 'There was an error during validation',
+      data: validation.error.details[0].message,
+      error: true,
+    });
+  }
+  return next();
+};
+
+export default {
+  validationCreateProject,
+};
