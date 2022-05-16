23759b3b15dc51ff7bb73f668af22feabe2cd8e1
NicolasFontana <nico.fontana12@gmail.com>
2022-05-16 16:09:30 -0300

    TG-8: generated routes and updated model project

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import projectsController from '../controllers/projects';
-// import projectValidation from '../validations/project';
+import projectValidation from '../validations/project';
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.get('/', projectsController.filter);
 
 router.get('/:id', projectsController.getById);
 
-router.post('/', projectsController.create);
+router.post('/', projectValidation.validationCreateProject, projectsController.create);
 
 router.put('/:id', projectsController.update);
 
