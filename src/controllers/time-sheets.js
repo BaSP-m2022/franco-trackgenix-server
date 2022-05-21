3b7e1934d1833a7af9c31e8623299f1a9e1a93ae
NicolasFontana <nico.fontana12@gmail.com>
2022-05-21 19:25:33 -0300

    TG-22: fixed errors

--- src/controllers/time-sheets.js
+++ src/controllers/time-sheets.js
@@ -68,7 +68,7 @@ const getById = async (req, res) => {
 
 const getAll = async (req, res) => {
   try {
-    const result = await TimeSheets.find(req.query).populate('tasks', { description: 0, workedHours: 0, projects: 0 });
+    const result = await TimeSheets.find(req.query).populate('tasks');
     if (result.length > 0) {
       return res.status(200).json({
         message: 'Time-sheets',
