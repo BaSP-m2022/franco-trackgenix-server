1b679c18c3ce2ccf6cbc2bdc60cf7177b1bd05b3
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2022-05-22 20:24:20 -0300

    TG-29: add unit Testing to getAll and delete

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -1,18 +1,47 @@
 import request from 'supertest';
-import Project from '../models/Projects';
-import projectsSeeds from '../seeds/projects';
+import TimeSheet from '../models/Time-sheets';
+import timeSheetSeeds from '../seeds/time-sheets';
 import app from '../app';
 
+const timeSheetId = '628a97a1e1c673704bbfa91b';
 beforeAll(async () => {
-  await Project.collection.insertMany(projectsSeeds);
+  await TimeSheet.collection.insertMany(timeSheetSeeds);
 });
 
-describe('Test Projects routes', () => {
-  test('It should get the project list', async () => {
-    const response = await request(app).get('/projects');
-    expect(response.body.message).toBe('Project found');
+describe('GET/timeSheet', () => {
+  test('It should get the TimeSheet list', async () => {
+    const response = await request(app).get('/time-sheets');
     expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe('Time-sheets');
     expect(response.body.data.length).toBeGreaterThan(0);
     expect(response.body.error).toBe(false);
+  });
+  test('It should get 404', async () => {
+    const response = await request(app).get('/time-sheets?totalHours=564125032064620240968468451');
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('A valid parameter is needed');
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).not.toEqual(expect.anything());
+  });
+});
+
+describe('DELETE/timeSheet/:id', () => {
+  test('should delete', async () => {
+    const response = await request(app).delete(`/time-sheets/${timeSheetId}`).send();
+    expect(response.statusCode).toBe(200);
+  });
+  test('It should get 404', async () => {
+    const response = await request(app).delete('/time-sheets/628a979a3661749f792a55c3');
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('Time-sheet was not found');
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).not.toEqual(expect.anything());
+  });
+  test('It should get 500', async () => {
+    const response = await request(app).delete('/time-sheets/1');
+    expect(response.statusCode).toBe(500);
+    expect(response.body.message).toBe('Time-sheet could not be deleted');
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).not.toEqual(expect.anything());
   });
 });
