8e28248ac628250e0fa190a6c2e8ff174abf4236
Juan Diego Auil <juandiauil@gmail.com>
2022-05-22 12:49:31 -0300

    TG-26: seed project

--- src/seeds/projects.js
+++ src/seeds/projects.js
@@ -1,16 +1,74 @@
 import mongoose from 'mongoose';
 
-export default [{
-  _id: mongoose.Types.ObjectId('62817f227d1213af0d9d1ab8'),
-  name: 'multi',
-  status: 'active',
-  description: 'asd',
-  employees: [
-    {
-      rate: 123,
-      role: 'role1',
-      _id: mongoose.Types.ObjectId('62817f227d1213af0d9d1ab9'),
-    },
-  ],
-  startDate: '2002-12-09T00:00:00.000+00:00',
-}];
+export default [
+  {
+    _id: mongoose.Types.ObjectId('628a59c9f67d45161581886f'),
+    name: 'Radium Rocket',
+    status: 'active',
+    description: 'BaSP-TG-26: radium',
+    employees: [
+      {
+        rate: 1,
+        role: 'QA',
+        _id: mongoose.Types.ObjectId('628a5ad8f6a8cdd3cefb2b07'),
+      },
+    ],
+    startDate: '2002-12-09T00:00:00.000+00:00',
+  },
+  {
+    _id: mongoose.Types.ObjectId('628a59f1a847f0ea55251cc2'),
+    name: 'Facebook',
+    status: 'inactive',
+    description: 'BaSP-TG-26: facebook',
+    employees: [
+      {
+        rate: 2,
+        role: 'DEV',
+        _id: mongoose.Types.ObjectId('628a5ae1c56212ee7a5618cd'),
+      },
+    ],
+    startDate: '2002-12-09T00:00:00.000+00:00',
+  },
+  {
+    _id: mongoose.Types.ObjectId('628a5a1d3a9e2db5eeebf009'),
+    name: 'Twitter',
+    status: 'active',
+    description: 'BaSP-TG-26: twitter',
+    employees: [
+      {
+        rate: 3,
+        role: 'PM',
+        _id: mongoose.Types.ObjectId('628a5ae941697271968cd673'),
+      },
+    ],
+    startDate: '2002-12-09T00:00:00.000+00:00',
+  },
+  {
+    _id: mongoose.Types.ObjectId('628a5a47f97f778e395a8b9a'),
+    name: 'Instagram',
+    status: 'active',
+    description: 'BaSP-TG-26: instagram',
+    employees: [
+      {
+        rate: 4,
+        role: 'QA',
+        _id: mongoose.Types.ObjectId('628a5af52a91d6871a6ebb81'),
+      },
+    ],
+    startDate: '2002-12-09T00:00:00.000+00:00',
+  },
+  {
+    _id: mongoose.Types.ObjectId('628a5a6d243774d9ac3969f7'),
+    name: 'Snapchat',
+    status: 'inactive',
+    description: 'BaSP-TG-26: snapchat',
+    employees: [
+      {
+        rate: 5,
+        role: 'PM',
+        _id: mongoose.Types.ObjectId('628a5b0333675625aa393918'),
+      },
+    ],
+    startDate: '2002-12-09T00:00:00.000+00:00',
+  },
+];
