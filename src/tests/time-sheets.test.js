a0881d0e838aeb4a23b723032c8f8d6d1db7f4c8
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2022-05-23 15:02:26 -0300

    TG-29: finish to put on unit testing

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -78,12 +78,109 @@ describe('PUT/timeSheet/:id', () => {
     expect(response.body.error).toBe(false);
     expect(response.body.data).toEqual(expect.anything());
   });
-  test('It should get status 500', async () => {
+  test('It should get status 404', async () => {
+    const response = await request(app).delete('/time-sheets/628a979a3661749f792a55c3');
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('Time-sheet was not found');
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).toBe(undefined);
+  });
+  test('should put status 500', async () => {
     const response = await request(app).get('/time-sheets/215561');
     expect(response.statusCode).toBe(500);
     expect(response.body.message).toBe('Time-sheet was not found');
     expect(response.body.error).toBe(true);
     expect(response.body.data).toBe(undefined);
+  });
+  test('It should get status 400 and task empty', async () => {
+    const response = await request(app).put(`/time-sheets/${timeSheetId}`).send(
+      {
+        tasks: [''],
+        totalHours: 13,
+        status: 'active',
+        startDate: '2021-12-12T00:00:00.000+00:00',
+        endDate: '2022-12-12T00:00:00.000+00:00',
+        employeeId: '60d4a32f257e066e9495ce12',
+      },
+    );
+    expect(response.body.message).toBe('An error ocurred');
+    expect(response.statusCode).toBe(500);
+    expect(response.body.error).toBe(true);
+  });
+  test('It should get status 400 and total hours empty', async () => {
+    const response = await request(app).put(`/time-sheets/${timeSheetId}`).send(
+      {
+        tasks: ['6289a9f3c375d9047b94a4c5'],
+        totalHours: '',
+        status: 'active',
+        startDate: '2021-12-12T00:00:00.000+00:00',
+        endDate: '2022-12-12T00:00:00.000+00:00',
+        employeeId: '60d4a32f257e066e9495ce12',
+      },
+    );
+    expect(response.body.message).toBe('There was an error during validation');
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe(true);
+  });
+  test('It should get status 400 and status incorrect', async () => {
+    const response = await request(app).put(`/time-sheets/${timeSheetId}`).send(
+      {
+        tasks: ['6289a9f3c375d9047b94a4c5'],
+        totalHours: 13,
+        status: 15154,
+        startDate: '2021-12-12T00:00:00.000+00:00',
+        endDate: '2022-12-12T00:00:00.000+00:00',
+        employeeId: '60d4a32f257e066e9495ce12',
+      },
+    );
+    expect(response.body.message).toBe('There was an error during validation');
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe(true);
+  });
+  test('It should get status 400 and start date empty', async () => {
+    const response = await request(app).put(`/time-sheets/${timeSheetId}`).send(
+      {
+        tasks: ['6289a9f3c375d9047b94a4c5'],
+        totalHours: 13,
+        status: 'active',
+        startDate: '',
+        endDate: '2022-12-12T00:00:00.000+00:00',
+        employeeId: '60d4a32f257e066e9495ce12',
+      },
+    );
+    expect(response.body.message).toBe('There was an error during validation');
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe(true);
+  });
+  test('It should get status 400 and end date empty', async () => {
+    const response = await request(app).put(`/time-sheets/${timeSheetId}`).send(
+      {
+        tasks: ['6289a9f3c375d9047b94a4c5'],
+        totalHours: 13,
+        status: 'active',
+        startDate: '2021-12-12T00:00:00.000+00:00',
+        endDate: '',
+        employeeId: '60d4a32f257e066e9495ce12',
+      },
+    );
+    expect(response.body.message).toBe('There was an error during validation');
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe(true);
+  });
+  test('It should get status 400 and employeeId empty', async () => {
+    const response = await request(app).put(`/time-sheets/${timeSheetId}`).send(
+      {
+        tasks: ['6289a9f3c375d9047b94a4c5'],
+        totalHours: 13,
+        status: 'active',
+        startDate: '2021-12-12T00:00:00.000+00:00',
+        endDate: '2022-12-12T00:00:00.000+00:00',
+        employeeId: '',
+      },
+    );
+    expect(response.body.message).toBe('There was an error during validation');
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe(true);
   });
 });
 
