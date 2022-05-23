ee8a4b3306e764fce217502cba1576e4fa8c80c0
Gonzalo Collares <collares.gonzalo@gmail.com>
2022-05-23 00:11:48 -0300

    TG-33: create branch and routes test

--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -1,18 +1,67 @@
 import request from 'supertest';
-import Project from '../models/Projects';
-import projectsSeeds from '../seeds/projects';
+import SuperAdmins from '../models/Super-admins';
+import superAdminsSeed from '../seeds/super-admins';
 import app from '../app';
 
 beforeAll(async () => {
-  await Project.collection.insertMany(projectsSeeds);
+  await SuperAdmins.collection.insertMany(superAdminsSeed);
 });
 
-describe('Test Projects routes', () => {
-  test('It should get the project list', async () => {
-    const response = await request(app).get('/projects');
-    expect(response.body.message).toBe('Project found');
+const superAdminId = '246c3e8616a6374cae43b660';
+
+describe('GET /super-admins', () => {
+  test('get super admins list', async () => {
+    const response = await request(app).get('/super-admins');
+    expect(response.body.message).toBe('Super Admins found successfully');
     expect(response.statusCode).toBe(200);
     expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('GET by id /super-admins/:id', () => {
+  test('get super admin by id ', async () => {
+    const response = await request(app).get(`/super-admins/${superAdminId}`);
+    expect(response.body.message).toBe('Super Admin found successfully');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.data).toBeDefined();
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('POST /super-admins', () => {
+  test('create super admin', async () => {
+    const response = await request(app).post('/super-admins').send({
+      password: '1e4c62d434',
+      email: 'mainline2@carkannino.com',
+      firstName: 'Marcos',
+      lastName: 'Kannino',
+    });
+    expect(response.body.message).toBe('Super Admin added to database');
+    expect(response.statusCode).toBe(201);
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('PUT /super-admins/:id', () => {
+  test('edit super admin', async () => {
+    const response = await request(app).put(`/super-admins/${superAdminId}`).send({
+      password: '1e4c62d4c4',
+      email: 'teffuedita2@carkannino.com',
+      firstName: 'Teffu',
+      lastName: 'Isteditado',
+    });
+    expect(response.body.message).toBe('Super Admin edited successfully');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('DELETE /super-admins/:id', () => {
+  test('delete super admin', async () => {
+    const response = await request(app).delete(`/super-admins/${superAdminId}`);
+    expect(response.body.message).toBe('Super Admin deleted successfully');
+    expect(response.statusCode).toBe(200);
     expect(response.body.error).toBe(false);
   });
 });
