6818dfd30c9de42be5aa5020654013905d1ccb43
FlorenciaEtchart <floretchart04@gmail.com>
2022-05-24 01:32:10 +0200

    TG: updating my branch

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -7,18 +7,32 @@ beforeAll(async () => {
   await Employee.collection.insertMany(employeeSeeds);
 });
 
-const employeeId = '60d4a32f257e066e9495ce12';
+// const employeeId = '60d4a32f257e066e9495ce12';
 
 describe('GET/employees', () => {
   test('It should get the employees list', async () => {
     const response = await request(app).get('/employees');
-    expect(response.body.message).toBe('Employee found successfully');
+    expect(response.body.message).toBe('Success');
     expect(response.statusCode).toBe(200);
     expect(response.body.data.length).toBeGreaterThan(0);
     expect(response.body.error).toBe(false);
   });
+  test('It should return a 404 status', async () => {
+    const response = await request(app).get('/employees?firstName=Pepito');
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('Employee was not found');
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).toBe(undefined);
+  });
+  test('It should return a 500 status', async () => {
+    const response = await request(app).get('/employees/12345');
+    expect(response.statusCode).toBe(500);
+    expect(response.body.message).toBe('error');
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).toBe(undefined);
+  });
 });
-
+/*
 describe('GET by id/employees/:id', () => {
   test('It should get an employee by id', async () => {
     const response = await request(app).get(`/employees/${employeeId}`);
@@ -32,28 +46,48 @@ describe('GET by id/employees/:id', () => {
 describe('POST/employees', () => {
   test('It should create an employee', async () => {
     const response = await request(app).post('employee').send({
-      password: 'test123',
-      email: 'esteban.frare@radiumrocket.com',
       firstName: 'Esteban',
       lastName: 'Frare',
+      dni: '38240915',
+      email: 'esteban.frare@radiumrocket.com',
+      password: 'test123',
+      dateOfBirth: '03-11-1987',
     });
     expect(response.body.message).toBe('Employee added to database');
     expect(response.statusCode).toBe(201);
     expect(response.body.error).toBe(false);
   });
+  test('404 error because the email entered is already in use', async () => {
+    const response = await request(app).post('employee').send({
+      firstName: 'Esteban',
+      lastName: 'Frare',
+      dni: '38240915',
+      email: 'esteban.frare@radiumrocket.com',
+      password: 'test123',
+      dateOfBirth: '03-11-1987',
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toEqual('An employee with this email already exists');
+    expect(response.body.data).toBeUndefined();
+  });
 });
 
-describe('PUT/employees', () => {
-  test('It should edit an employee', async () => {
-    const response = await request(app).put('employee').send({
-      password: 'Holaprob123',
-      email: 'totalfake@whatever.com',
-      firstName: 'Totito',
-      lastName: 'Chulito',
-    });
-    expect(response.body.message).toBe('Employee edited successfully');
-    expect(response.statusCode).toBe(201);
+describe('PUT/employees/:id', () => {
+  test('It should delete an employee', async () => {
+    const response = await request(app).put(`/employees/${employeeId}`).send(
+      {
+        firstName: 'Totito',
+        lastName: 'Chulito',
+        dni: '33016244',
+        email: 'totalfake@whatever.com',
+        password: 'Holaprob123',
+        dateOfBirth: '05-05-1985',
+      },
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe('Employee edited');
     expect(response.body.error).toBe(false);
+    expect(response.body.data).toEqual(expect.anything());
   });
 });
 
@@ -65,3 +99,4 @@ describe('DELETE/employee', () => {
     expect(response.body.error).toBe(false);
   });
 });
+*/
