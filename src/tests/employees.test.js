968c936de8a92734657abc8161e94d67e86b4761
FlorenciaEtchart <floretchart04@gmail.com>
2022-05-23 14:57:23 +0200

    TG: employees base of testing was made

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -1,18 +1,67 @@
 import request from 'supertest';
-import Project from '../models/Projects';
-import projectsSeeds from '../seeds/projects';
+import Employee from '../models/Employees';
+import employeeSeeds from '../seeds/employees';
 import app from '../app';
 
 beforeAll(async () => {
-  await Project.collection.insertMany(projectsSeeds);
+  await Employee.collection.insertMany(employeeSeeds);
 });
 
-describe('Test Projects routes', () => {
-  test('It should get the project list', async () => {
-    const response = await request(app).get('/projects');
-    expect(response.body.message).toBe('Project found');
+const employeeId = '60d4a32f257e066e9495ce12';
+
+describe('GET/employees', () => {
+  test('It should get the employees list', async () => {
+    const response = await request(app).get('/employees');
+    expect(response.body.message).toBe('Employee found successfully');
     expect(response.statusCode).toBe(200);
     expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('GET by id/employees/:id', () => {
+  test('It should get an employee by id', async () => {
+    const response = await request(app).get(`/employees/${employeeId}`);
+    expect(response.body.message).toBe('Employee found successfully');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('POST/employees', () => {
+  test('It should create an employee', async () => {
+    const response = await request(app).post('employee').send({
+      password: 'test123',
+      email: 'esteban.frare@radiumrocket.com',
+      firstName: 'Esteban',
+      lastName: 'Frare',
+    });
+    expect(response.body.message).toBe('Employee added to database');
+    expect(response.statusCode).toBe(201);
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('PUT/employees', () => {
+  test('It should edit an employee', async () => {
+    const response = await request(app).put('employee').send({
+      password: 'Holaprob123',
+      email: 'totalfake@whatever.com',
+      firstName: 'Totito',
+      lastName: 'Chulito',
+    });
+    expect(response.body.message).toBe('Employee edited successfully');
+    expect(response.statusCode).toBe(201);
+    expect(response.body.error).toBe(false);
+  });
+});
+
+describe('DELETE/employee', () => {
+  test('It should delete an employee', async () => {
+    const response = await request(app).delete(`/employees/${employeeId}`);
+    expect(response.body.message).toBe('Employee deleted successfully');
+    expect(response.statusCode).toBe(200);
     expect(response.body.error).toBe(false);
   });
 });
